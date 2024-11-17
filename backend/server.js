3d4bcf57efb41bbd73e3d58ec949885f80e79134
Imran Ali <imranranjha3464@gmail.com>
2024-11-16 19:37:04 -0700

    Fix environment variables for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3001;
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST,
   user: process.env.DB_USER ,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
